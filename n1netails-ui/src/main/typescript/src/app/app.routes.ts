c0b7b5bea06e5de67931ce4ce63e7aac86158b5d
Shahid Foy <shahidfoy@gmail.com>
2025-05-18 18:03:25 -0600

    adding edit profile component

--- n1netails-ui/src/main/typescript/src/app/app.routes.ts
+++ n1netails-ui/src/main/typescript/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES) },
+  { path: 'edit-profile', component: EditProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) },
--- n1netails-ui/src/main/typescript/src/app/shared/template/sidenav/sidenav.component.ts
+++ n1netails-ui/src/main/typescript/src/app/shared/template/sidenav/sidenav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [NzMenuModule],
+  imports: [NzMenuModule,RouterModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.less'
 })

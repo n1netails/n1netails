57cc661fe64b4c2f091f8910b3bb0db2efaa559d
Shahid Foy <shahidfoy@gmail.com>
2025-08-05 17:15:33 -0600

    handling notes logic to not safe notes if no content provided

---
+++ n1netails-api/src/main/java/com/n1netails/n1netails/api/exception/type/NoteNoContentException.java
@@ -0,0 +1,7 @@
+package com.n1netails.n1netails.api.exception.type;
+
+public class NoteNoContentException extends Exception {
+    public NoteNoContentException(String message) {
+        super(message);
+    }
+}

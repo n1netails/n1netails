09e1f428988d7166946379aeb4a1adb8883ef60d
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-08-05 12:38:31 +0530

    refactor: use UUID for user ID generation instead of custom random string

--- n1netails-api/src/main/java/com/n1netails/n1netails/api/util/UserUtil.java
+++ n1netails-api/src/main/java/com/n1netails/n1netails/api/util/UserUtil.java
@@ -1,15 +1,12 @@
 package com.n1netails.n1netails.api.util;
 
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.text.RandomStringGenerator;
 
 @Slf4j
 public class UserUtil {
 
     public static String generateUserId() {
-        RandomStringGenerator generator = new RandomStringGenerator.Builder()
-                .withinRange('0', '9')
-                .build();
-        return generator.generate(10);
+        return UUID.randomUUID().toString();
     }
 }

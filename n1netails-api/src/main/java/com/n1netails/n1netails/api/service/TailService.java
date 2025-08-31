a632340d5d9c9650fc97b9783abf59b9756d0266
Shahid Foy <shahidfoy@gmail.com>
2025-08-31 11:34:27 -0600

    refactoring code in bookmark service

--- n1netails-api/src/main/java/com/n1netails/n1netails/api/service/TailService.java
+++ n1netails-api/src/main/java/com/n1netails/n1netails/api/service/TailService.java
@@ -2,6 +2,7 @@ package com.n1netails.n1netails.api.service;
 
 import com.n1netails.n1netails.api.exception.type.*;
 import com.n1netails.n1netails.api.model.core.TailStatus;
+import com.n1netails.n1netails.api.model.dto.TailSummary;
 import com.n1netails.n1netails.api.model.request.ResolveTailRequest;
 import com.n1netails.n1netails.api.model.request.TailPageRequest;
 import com.n1netails.n1netails.api.model.response.TailResponse;
@@ -13,10 +14,18 @@ import java.util.List;
 public interface TailService {
 
     Page<TailResponse> getTails(TailPageRequest request, UserPrincipal currentUser) throws TailStatusNotFoundException, TailTypeNotFoundException, TailLevelNotFoundException;
+
+    List<String> getTailLevels(TailPageRequest request) throws TailLevelNotFoundException;
+    List<String> getTailTypes(TailPageRequest request) throws TailTypeNotFoundException;
+    List<String> getTailStatuses(TailPageRequest request) throws TailStatusNotFoundException;
+
+
     List<TailResponse> getTop9NewestTails(UserPrincipal currentUser);
     TailResponse getTailById(Long id, UserPrincipal currentUser) throws TailNotFoundException, UnauthorizedException;
 
     TailResponse updateStatus(Long id, TailStatus tailStatus, UserPrincipal currentUser) throws TailNotFoundException, UnauthorizedException;
 
     void markResolved(ResolveTailRequest request, UserPrincipal currentUser) throws TailNotFoundException, TailStatusNotFoundException, UnauthorizedException;
+
+    TailResponse setTailSummaryResponse(TailSummary tailSummary);
 }
--- n1netails-api/src/main/java/com/n1netails/n1netails/api/service/impl/TailBookmarkServiceImpl.java
+++ n1netails-api/src/main/java/com/n1netails/n1netails/api/service/impl/TailBookmarkServiceImpl.java
@@ -17,11 +17,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.n1netails.n1netails.api.util.UserUtil.isInN1netailsOrg;
 
 @Slf4j
 @Service
@@ -32,9 +28,6 @@ public class TailBookmarkServiceImpl implements TailBookmarkService {
     private final TailBookmarkRepository tailBookmarkRepository;
     private final UserRepository userRepository;
     private final TailRepository tailRepository;
-    private final TailLevelRepository levelRepository;
-    private final TailTypeRepository typeRepository;
-    private final TailStatusRepository statusRepository;
 
 
     @Override
@@ -64,50 +57,12 @@ public class TailBookmarkServiceImpl implements TailBookmarkService {
     @Transactional(readOnly = true)
     public Page<TailResponse> getUserBookmarks(TailPageRequest request, UserPrincipal currentUser) throws TailTypeNotFoundException, TailLevelNotFoundException, TailStatusNotFoundException {
 
-
-        // Authorization logic implemented. Users can only see tails related to their organizations.
-        // If a user is part of the n1netails organization, they can only view their own tails.
-        // UserPrincipal currentUser = authorizationService.getCurrentUserPrincipal(); // Removed
-        List<Long> organizationIds = currentUser.getOrganizations().stream().map(OrganizationEntity::getId).toList();
-
         Pageable pageable = PageRequest.of(request.getPage(), request.getSize());
-
         String searchTerm = request.getSearchTerm() == null || request.getSearchTerm().isEmpty() ? "" : request.getSearchTerm();
 
-        List<String> statuses;
-        if (request.getFilterByStatus() == null || request.getFilterByStatus().isEmpty()) {
-            List<TailStatusEntity> statusEntities = this.statusRepository.findAll();
-            statuses = statusEntities.stream().map(TailStatusEntity::getName).toList();
-        } else {
-            TailStatusEntity tailStatusEntity = this.statusRepository.findTailStatusByName(request.getFilterByStatus())
-                    .orElseThrow(() -> new TailStatusNotFoundException("Requested status name does not exist."));
-            statuses = List.of(tailStatusEntity.getName());
-        }
-        log.info("STATUS NAMES: {}", statuses);
-
-        List<String> types;
-        if (request.getFilterByType() == null || request.getFilterByType().isEmpty()) {
-            List<TailTypeEntity> tailTypeEntities = this.typeRepository.findAll();
-            types = tailTypeEntities.stream().map(TailTypeEntity::getName).toList();
-        } else {
-            TailTypeEntity tailTypeEntity = this.typeRepository.findTailTypeByName(request.getFilterByType())
-                    .orElseThrow(() -> new TailTypeNotFoundException("Requested type name does not exist."));
-            types = List.of(tailTypeEntity.getName());
-        }
-        log.info("TYPE NAMES: {}", types);
-
-        List<String> levels;
-        if (request.getFilterByLevel() == null || request.getFilterByLevel().isEmpty()) {
-            List<TailLevelEntity> tailLevelEntities = this.levelRepository.findAll();
-            levels = tailLevelEntities.stream().map(TailLevelEntity::getName).toList();
-        } else {
-            TailLevelEntity tailLevelEntity = this.levelRepository.findTailLevelByName(request.getFilterByLevel())
-                    .orElseThrow(() -> new TailLevelNotFoundException("Requested level name does not exist."));
-            levels = List.of(tailLevelEntity.getName());
-        }
-        log.info("LEVEL NAMES: {}", levels);
-
-//        boolean isN1netails = isInN1netailsOrg(currentUser);
+        List<String> statuses = tailService.getTailStatuses(request);
+        List<String> types = tailService.getTailTypes(request);
+        List<String> levels = tailService.getTailLevels(request);
 
         Page<TailSummary> tailBookmarkPage;
 
@@ -126,38 +81,7 @@ public class TailBookmarkServiceImpl implements TailBookmarkService {
           pageable
         );
 
-        return tailBookmarkPage.map(this::setTailSummaryResponse);
-
-
-
-        //        // todo handle page request
-//        // load tails by search filter then only display bookmarked tails
-//
-//        // todo this need to go first before this.tailService.getTails
-//        List<TailBookmarkEntity> bookmarks = tailBookmarkRepository.findByUserId(currentUser.getId());
-//
-//        List<TailEntity> bookmarksTailEntity = bookmarks.stream()
-//                .map(TailBookmarkEntity::getTail)
-//                .toList();
-//        List<TailResponse> bookmarksTailResponse = bookmarksTailEntity.stream()
-//                .map(this::toTailResponse)
-//                .toList();
-//
-//
-//        Page<TailResponse> pagedTails = this.tailService.getTails(request, currentUser);
-//
-//        // todo implement filter to remove data that is not bookmarked
-//
-////        Page<TailResponse> bookmarkedPagedTails = pagedTails.stream()
-////                .filter(tail -> tail.getId() == )
-////                .toList();
-//
-//        return null;
-//
-//        // todo filter out tails that are not bookmarked
-////        return bookmarks.stream()
-////                .map(TailBookmarkEntity::getTail)
-////                .collect(Collectors.toList());
+        return tailBookmarkPage.map(tailService::setTailSummaryResponse);
     }
 
     @Override
@@ -165,39 +89,4 @@ public class TailBookmarkServiceImpl implements TailBookmarkService {
     public boolean isBookmarked(Long userId, Long tailId) {
         return tailBookmarkRepository.existsByUserIdAndTailId(userId, tailId);
     }
-
-    private TailResponse setTailSummaryResponse(TailSummary tailSummary) {
-        TailResponse tailResponse  = new TailResponse();
-        tailResponse.setId(tailSummary.getId());
-        tailResponse.setTitle(tailSummary.getTitle());
-        tailResponse.setDescription(tailSummary.getDescription());
-        tailResponse.setTimestamp(tailSummary.getTimestamp());
-        tailResponse.setResolvedTimestamp(tailSummary.getResolvedTimestamp());
-        tailResponse.setAssignedUserId(tailSummary.getAssignedUserId());
-        UsersEntity user = userRepository.findUserById(tailSummary.getAssignedUserId());
-        tailResponse.setAssignedUsername(user.getUsername());
-        tailResponse.setLevel(tailSummary.getLevel());
-        tailResponse.setType(tailSummary.getType());
-        tailResponse.setStatus(tailSummary.getStatus());
-        return tailResponse;
-    }
-
-    // todo use this to set tail response
-//    private TailResponse toTailResponse(TailEntity tailEntity) {
-//        return new TailResponse(
-//                tailEntity.getId(),
-//                tailEntity.getTitle(),
-//                tailEntity.getDescription(),
-//                tailEntity.getTimestamp(),
-//                tailEntity.getResolvedTimestamp(),
-//                tailEntity.getAssignedUserId(),
-//                null, // assignedUsername is not in TailEntity, so I'll leave it null
-//                tailEntity.getDetails(),
-//                tailEntity.getLevel() != null ? tailEntity.getLevel().getName() : null,
-//                tailEntity.getType() != null ? tailEntity.getType().getName() : null,
-//                tailEntity.getStatus() != null ? tailEntity.getStatus().getName() : null,
-//                null, // metadata is not in TailEntity
-//                tailEntity.getOrganization() != null ? tailEntity.getOrganization().getId() : null
-//        );
-//    }
 }
